8e05c6e2f149eff76674edf94749dece9cc53626
u00030614sise <u00030614@sise.com.pe>
2024-12-14 14:47:19 -0500

    EV2

---
+++ src/test/java/com/example/Trabajadores/TrabajadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trabajadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
